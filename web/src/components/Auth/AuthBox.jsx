651a663b2fa754cdc285154f762ab6dc549f5c16
VITALI TUMASHEUSKI <unidiag1@gmail.com>
2026-07-05 21:05:42 +0300

    Remove ignored files

--- web/src/components/Auth/AuthBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import LoginForm from "./LoginForm";
-
-
-export default function AuthBox({ onSuccess }) {
-  return <LoginForm onSwitch={"login"} onSuccess={onSuccess} />;
-}
